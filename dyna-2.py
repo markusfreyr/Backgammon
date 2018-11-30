394569a9eb3ee55f51a11f4e660ae1b8e70e81e3
Freyr Danielsson <freyrdanielsson@gmail.com>
2018-11-30 21:08:39 +0000

    name fix

--- dyna-2.py
+++ dyna-2.py
@@ -84,10 +84,10 @@ def Q(features, player):
 def action(board, dice, player, i):
 
 	doubleD = 1 if (dice[0]==dice[1] and i == 0) else 0
-	if player == -1: board_copy = flipped_agent.flip_board(np.copy(board_copy))
+	if player == -1: board = flipped_agent.flip_board(np.copy(board))
 	
 	# check out the legal moves available for the throw
-	possible_moves, possible_boards = Backgammon.legal_moves(board_copy, dice, player=1)
+	possible_moves, possible_boards = Backgammon.legal_moves(board, dice, player=1)
 
 	# if there are no moves available
 	if len(possible_moves) == 0:
@@ -124,111 +124,97 @@ def action(board, dice, player, i):
 	return possible_moves[np.argmax(na)] """
 
 def search(board,player):
-	global theta1, theta_trans1, theta2, theta_trans2
+	global w1_trans,b1_trans,w2_trans,b2_trans,w3_trans,b3_trans
+	xold1 = []
+	xold2 = []
 	start = time.time()
 	time_available = 1
-	original_board = np.copy(board)
+
 	if Backgammon.game_over(board):
 		return
-	while time_available - (time.time() - start) > 0:
-		board = np.copy(original_board)
-		z_trans1 = np.zeros(196)
-		z_trans2 = np.zeros(196)
-
-		dice = Backgammon.roll_dice()
-		a1 = action(np.copy(board), dice, player, 0) #find action for p1
-		if player == 1:
-			board1_old = np.copy(board) # Keep board for p1
-		else:
-			board2_old = np.copy(board)
-		for i in range(1+int(dice[0] == dice[1])):
-			# update the board, take action a1
-			if len(a1) != 0:
-				for m in a1:
-					board = Backgammon.update_board(board, m, player)
-		reward = 0
-		if Backgammon.game_over(board):
-			return
-
-		# do same for p2
-		player = player*-1
-		dice = Backgammon.roll_dice()
-		a2 = action(np.copy(board), dice, player, 0)
-		if player == 1:
-			board1_old = np.copy(board)
-		else:
-			board2_old = np.copy(board)
-		for i in range(1+int(dice[0] == dice[1])):
-			# update the board
-			if len(a2) != 0:
-				for m in a2:
-					board = Backgammon.update_board(board, m, player)
-		if Backgammon.game_over(board):
-			return
+	t=0
+	#while time_available - (time.time() - start) > 0:
+	while t <= 30 and time_available - (time.time() - start) > 0:
+		t = t+1
 		
-		# change players again
-		player = player*-1
+		# Eligibility
+		z1_w1 = Variable(torch.zeros((4*24,197), device = device, dtype=torch.float))
+		z1_b1 = Variable(torch.zeros((4*24,1), device = device, dtype=torch.float))
+		z1_w2 = Variable(torch.zeros((2*24,4*24), device = device, dtype=torch.float))
+		z1_b2 = Variable(torch.zeros((2*24,1), device = device, dtype=torch.float))
+		z1_w3 = Variable(torch.zeros((1,2*24), device = device, dtype=torch.float))
+		z1_b3 = Variable(torch.zeros((1,1), device = device, dtype=torch.float))
+
+		z2_w1 = Variable(torch.zeros((4*24,197), device = device, dtype=torch.float))
+		z2_b1 = Variable(torch.zeros((4*24,1), device = device, dtype=torch.float))
+		z2_w2 = Variable(torch.zeros((2*24,4*24), device = device, dtype=torch.float))
+		z2_b2 = Variable(torch.zeros((2*24,1), device = device, dtype=torch.float))
+		z2_w3 = Variable(torch.zeros((1,2*24), device = device, dtype=torch.float))
+		z2_b3 = Variable(torch.zeros((1,1), device = device, dtype=torch.float))
+
 		dice = Backgammon.roll_dice()
-		a = action(np.copy(board), dice, player, 0) # find action for p1
-		counting_now = 0
-		while not Backgammon.game_over(board):
-			print("count")
-			print(counting_now)
-			counting_now += 1
+		a = action(np.copy(board), dice, player, 0)
 
-			print()
-			print(board)
-			print(a)
-
-
+		count = 0
+		first = True;
+		while not Backgammon.game_over(board) and not Backgammon.check_for_error(board):
 			for i in range(1+int(dice[0] == dice[1])):
-				board_new = np.copy(board) 
+				count += 1 if i != 1 else count + 0
+				if first and dice[0] == dice[1]: i + 1
+				first = False
+
+				#Execute action
+				new_board = np.copy(board) # new board is old board if no move
+				for m in a:
+					new_board = Backgammon.update_board(new_board, m, player)
+
+				reward = 1 if Backgammon.game_over(new_board) else 0
+
+				switch = 1 if i == 0 and dice[0] == dice[1] else -1
+				
+				#action for next player or same if double roll
+				doubleD = 1 if switch == 1 else 0
+				dice = Backgammon.roll_dice()
+				new_action = action(new_board, dice, player*switch,i)
+				
+				if count > 3:
+					if player == 1:
+						x = Variable(torch.tensor(feature_encoding(board, doubleD), dtype=torch.float, device = device)).view(197,1)
+						y = forward(x)
+						yold = forward(xold1)
+						delta = reward + y - yold # TD-error
+						
+						# update theta
+						thetaUpdate_trans(z1_w1,z1_b1,z1_w2,z1_b2,z1_w3,z1_b3, delta)
+						#elligibillyboy
+						z1_w1,z1_b1,z1_w2,z1_b2,z1_w3,z1_b3 = eligibillyUpdate(z1_w1,z1_b1,z1_w2,z1_b2,z1_w3,z1_b3, yold)
+
+					else:
+						# Flip?
+						flipped = flipped_agent.flip_board(np.copy(board))
+						x = Variable(torch.tensor(feature_encoding(flipped, doubleD), dtype=torch.float, device = device)).view(197,1)
+						y = forward(x)
+						yold = forward(xold2) # xold already flipped
+						delta = reward + y - yold # TD-error
+						
+						# update theta
+						thetaUpdate_trans(z2_w1,z2_b1,z2_w2,z2_b2,z2_w3,z2_b3, delta)
+						#elligibillyboy
+						z2_w1,z2_b1,z2_w2,z2_b2,z2_w3,z2_b3 = eligibillyUpdate(z2_w1,z2_b1,z2_w2,z2_b2,z2_w3,z2_b3, yold)
+
+				if player == 1:
+					xold1 = Variable(torch.tensor(feature_encoding(board, doubleD), dtype=torch.float, device = device)).view(197,1)
+				else:
+					#Flip?
+					flipped = flipped_agent.flip_board(np.copy(board))
+					xold2 = Variable(torch.tensor(feature_encoding(flipped, doubleD), dtype=torch.float, device = device)).view(197,1)
+
+				board = new_board
+				a = new_action
+
+				player = player * switch
 			
-				# update the board
-				if len(a) != 0:
-					for m in a:
-						print(m)
-						board_new = Backgammon.update_board(np.copy(board_new), m, player)
-						print(Backgammon.check_for_error(board_new))
 			
-			if Backgammon.check_for_error(board_new):
-				exit()
-
-
-			#board_new = A(board,a)
-			#reward = B(board,a)
-
-			if Backgammon.game_over(board_new):
-				reward = 1
-			else:
-				reward = 0
-
-			player = player*-1
-			
-			dice = Backgammon.roll_dice()
-			a_new = action(np.copy(board_new), dice, player, 0)
-			feature_new = feature_encoding(np.copy(board_new))
-
-			if player == 1:
-				feature_old = feature_encoding(np.copy(board1_old))
-			else:
-				feature_old = feature_encoding(np.copy(board2_old))
-			delta_trans = reward + Q_trans(feature_new,player) - Q_trans(feature_old,player)
-
-			if player == 1:
-				theta_trans1 = theta_trans1 + 0.01*delta_trans*z_trans1
-				z_trans1 = 0.5*z_trans1 + feature_old
-			else:
-				theta_trans2 = theta_trans2 + 0.01*delta_trans*z_trans2
-				z_trans2 = 0.5*z_trans2 + feature_old
-			
-			if player == 1:
-				board1_old = np.copy(board)
-			else:
-				board2_old = np.copy(board)
-
-			board = board_new
-			a = a_new
 
 def learn(n):
 	#global theta1, theta_trans1, theta2, theta_trans2
@@ -239,6 +225,7 @@ def learn(n):
 	#theta = 0 gert annarsstaðar
 	#while True:
 	for i in range(n):
+		print(i)
 		count = 0
 		xold1 = []
 		xold2 = []
@@ -267,41 +254,39 @@ def learn(n):
 
 		# initialize game
 		board = Backgammon.init_board()
-		state = feature_encoding(np.copy(board))
+		# state = feature_encoding(np.copy(board))
 		player = np.random.randint(2)*2-1 # which player begins?
 
-
-		dice = Backgammon.roll_dice() # first roll
-
-		z2 = 0
-		search(board,player)
+		search(np.copy(board),player)
 
 		# find action for the first player
+		dice = Backgammon.roll_dice() # first roll
 		a = action(np.copy(board), dice, player, 0)
 
 		first = True
-		while not Backgammon.game_over(board):
+		while not Backgammon.game_over(board) and not Backgammon.check_for_error(board):
 			for i in range(1+int(dice[0] == dice[1])):
 				count += 1 if i != 1 else count + 0
 				if first and dice[0] == dice[1]: i + 1 # if the first roll was double, take into account
 				first = False
 
 				#Execute action
-				new_board = []
+				new_board = np.copy(board)
 				for m in a:
-					new_board = Backgammon.update_board(np.copy(board), m, player)
+					new_board = Backgammon.update_board(new_board, m, player)
 
 				reward = 1 if Backgammon.game_over(new_board) else 0
 
-				search(new_board,player)
+				switch = 1 if i == 0 and dice[0] == dice[1] else -1
+
+				search(np.copy(new_board),player*switch)
 				
 				#action for next player or same if double roll
-				switch = 1 if i == 0 and dice[0] == dice[1] else -1
 				doubleD = 1 if switch == 1 else 0
 				dice = Backgammon.roll_dice()
 				new_action = action(new_board, dice, player*switch,i)
 				
-				if count > 2:
+				if count > 3:
 					if player == 1:
 						x = Variable(torch.tensor(feature_encoding(board, doubleD), dtype=torch.float, device = device)).view(197,1)
 						y = forward(x)
@@ -347,6 +332,15 @@ def thetaUpdate(zw1, zb1, zw2, zb2, zw3, zb3, delta):
 	w3 = w3 + 0.01* delta * zw3
 	b3 = b3 + 0.01* delta * zb3
 
+def thetaUpdate_trans(zw1, zb1, zw2, zb2, zw3, zb3, delta):
+	global w1_trans,b1_trans,w2_trans,b2_trans,w3_trans,b3_trans
+	w1_trans = w1_trans + 0.01* delta * zw1
+	b1_trans = b1_trans + 0.01* delta * zb1
+	w2_trans = w2_trans + 0.01* delta * zw2
+	b2_trans = b2_trans + 0.01* delta * zb2
+	w3_trans = w3_trans + 0.01* delta * zw3
+	b3_trans = b3_trans + 0.01* delta * zb3
+
 def eligibillyUpdate(zw1, zb1, zw2, zb2, zw3, zb3, phi):
 	zw1 = 0.7 * zw1 + phi
 	zb1 = 0.7 * zb1 + phi
@@ -358,12 +352,15 @@ def eligibillyUpdate(zw1, zb1, zw2, zb2, zw3, zb3, phi):
 	return zw1, zb1, zw2, zb2, zw3, zb3
 
 def load(name):
-	w1 = torch.load(name)
-	w2 = torch.load(name)
-	w3 = torch.load(name)
-	b1 = torch.load(name)
-	b2 = torch.load(name)
-	b3 = torch.load(name)
+	try:
+		w1 = torch.load(name)
+		w2 = torch.load(name)
+		w3 = torch.load(name)
+		b1 = torch.load(name)
+		b2 = torch.load(name)
+		b3 = torch.load(name)
+	except FileNotFoundError:
+		print('starting fresh')
 
 def save(name):
 	torch.save(w1, name)
@@ -376,9 +373,12 @@ def save(name):
 
 def main():
 	# load
-	# Run real exp to initialize model
+	load('dyna')
 	# learn
-	# save
+	learn(500)
+	save('dyna')
+	#save
+	#save('dyna')
 
 	learn(10)
 
